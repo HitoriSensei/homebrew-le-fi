c077f221321ce62341e4dcffb4e7521b5f54550a
Piotr Bosak <hitori@duck.com>
2026-08-01 17:45:37 +0200

    Update homepage URL in le-fi.rb

--- Formula/le-fi.rb
+++ Formula/le-fi.rb
@@ -1,6 +1,6 @@
 class LeFi < Formula
   desc "Send or receive a file across NATs using a Cloudflare Quick Tunnel"
-  homepage "https://git.piesekpokojowy.uk/Hitori/le-fi"
+  homepage "https://git.petedoes.it/Hitori/le-fi"
   url "https://git.petedoes.it/Hitori/le-fi/archive/v0.9.14.tar.gz"
   sha256 "6096d9666613b21596a1d1810c8d83d0ffbb00e32ae74eff12342c504811a05c" # see README.md in this tap for how to compute it
   license "Apache-2.0"
